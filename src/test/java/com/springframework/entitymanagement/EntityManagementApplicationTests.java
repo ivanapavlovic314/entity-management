6f8b29c03ce1133607ba94d84e0e7195688eaefe
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-20 22:22:28 +0100

    initial commit

---
+++ src/test/java/com/springframework/entitymanagement/EntityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.entitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
