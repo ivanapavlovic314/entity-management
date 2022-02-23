3b302afceb12463444c07888dccd14b8baaa6f89
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-23 19:32:45 +0100

    Thymeleaf commit 3

--- src/main/java/com/springframework/entitymanagement/controllers/BookController.java
+++ src/main/java/com/springframework/entitymanagement/controllers/BookController.java
@@ -3,7 +3,6 @@ package com.springframework.entitymanagement.controllers;
 import com.springframework.entitymanagement.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +16,9 @@ public class BookController {
 
     @RequestMapping("/books")
     public String getBooks(Model model){
+
         model.addAttribute("books", bookRepository.findAll());
+
         return "books/list";
     }
 }
--- src/main/java/com/springframework/entitymanagement/repositories/BookRepository.java
+++ src/main/java/com/springframework/entitymanagement/repositories/BookRepository.java
@@ -1,8 +1,9 @@
 package com.springframework.entitymanagement.repositories;
 
 import com.springframework.entitymanagement.domain.Book;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
 }
